e42f278d2d06919c1c720255aedbf29048f89d8f
Roger Salomonsson <medicorab@gmail.com>
2025-02-02 18:57:55 +0100

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+/*
 
-use melody_compiler::compiler;
+Parses and transform a text file.
+
+Input format description markup:
+<date=iso8601date> <time="M"|"K">: <systolic=u8>/<diastolic=u8> <pulse=u8>
+
+*/
+
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+#![deny(clippy::pedantic)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+#![deny(unused_must_use)]
 
 fn main() {
-    let source = r#"1 to 5 of "A";"#;
-    let output = compiler(source);
-    println!("{}", output.unwrap());
 }
